9cc7e7907026f3d0f7fc1afd2345e2e9462bbbc3
phchainilwong <phchainilwong@s.sfusd.edu>
2026-03-23 11:36:49 -0700

    almost done with CP4

--- Scripts/background_parallax.gd
+++ Scripts/background_parallax.gd
@@ -4,4 +4,4 @@ var parallax : float = 0.7
 @onready var player = $"../Player"
 
 func _process (delta):
-	global_position = player.global_position
+	global_position = player.global_position * parallax
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -15,6 +15,10 @@ var move_input : float
 
 @onready var sprite : Sprite2D = $Sprite
 @onready var anim : AnimationPlayer = $AnimationPlayer
+@onready var audio : AudioStreamPlayer = $AudioStreamPlayer
+
+var take_damage_sfx : AudioStream = preload("res://Audio/take_damage.wav")
+var coin_sfx : AudioStream = preload("res://Audio/coin.wav")
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -42,6 +46,7 @@ func _process (delta):
 	
 	if global_position.y > 200:
 		game_over()
+		game_over()
 		
 func _manage_animation_ ():
 	if not is_on_floor():
@@ -55,18 +60,24 @@ func take_damage (amount : int):
 	health -= amount
 	OnUpdateHealth.emit(health)
 	_damage_flash()
+	play_sound(take_damage_sfx)
 	
 	if health <= 0:
 		call_deferred("game_over")
 		
 func game_over ():
-	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
 
 func increase_score(amount : int):
 	PlayerStats.score += amount
 	OnUpdateScore.emit(PlayerStats.score)
+	play_sound(coin_sfx)
 	
 func _damage_flash ():
 	sprite.modulate = Color.RED
 	await get_tree().create_timer(0.05).timeout
 	sprite.modulate = Color.WHITE
+	
+func play_sound (sound : AudioStream):
+	audio.stream = sound
+	audio.play()
