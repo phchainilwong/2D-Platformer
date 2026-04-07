1ee04d1f3910ba097e4106612f9ab4f9840eaff5
phchainilwong <phchainilwong@s.sfusd.edu>
2026-04-07 12:03:50 -0700

    Platformer Expanded

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 @export var move_direction : Vector2
-@export var move_speed : float = 20
+@export var move_speed : float = 50
 
 @onready var start_pos : Vector2 = global_position
 @onready var target_pos : Vector2 = global_position + move_direction
